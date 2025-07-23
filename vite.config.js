f8ca687dc89c8b3c556126716f88ad859f782f41
Ddos-spec <setgraph69@gmail.com>
2025-07-23 17:01:03 +0700

    mantap

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: '/bumina/', // <-- Ini settingan penting buat GitHub Pages
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
