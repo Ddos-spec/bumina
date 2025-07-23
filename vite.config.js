4d4efb6ad42c17580b7b4e078a84a28b63e3ba7f
Ddos-spec <setgraph69@gmail.com>
2025-07-23 15:58:09 +0700

    fakof

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path' // <-- Import 'path' dari Node.js
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // INI BAGIAN PENTINGNYA:
-      // Kita bilang ke Vite: "Kalau ada import yang diawali '@',
-      // anggap itu sama dengan 'src'."
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  // Pastikan bagian 'resolve' yang kita tambah sebelumnya sudah dihapus.
+  // Kosongkan atau kembalikan ke versi original proyek lu.
 })
