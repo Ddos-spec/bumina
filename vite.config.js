35876ab01b791c301f6922a355f567a7efe589c2
Ddos-spec <setgraph69@gmail.com>
2025-07-23 21:37:04 +0700

    coba

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+// vite.config.js
+
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bumina/', // <-- Ini settingan penting buat GitHub Pages
+  // Ini settingan penting buat GitHub Pages
+  // Pastikan path ini sesuai dengan "homepage" di package.json dan "basename" di App.jsx Router lo.
+  // Harus ada trailing slash '/' di belakang path-nya.
+  base: '/bumina/', 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
