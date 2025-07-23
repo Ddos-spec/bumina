aa8b612b12ae964974754ea6c5a18271a87c259c
Ddos-spec <setgraph69@gmail.com>
2025-07-23 20:33:21 +0700

    bismillah

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SocialWidget from './components/SocialWidget';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomePage from './pages/HomePage'; // <-- Kita buat komponen HomePage
+import HomePage from './pages/HomePage';
 import Blog from './pages/Blog';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    // TAMBAHIN 'basename' DI SINI AJA, BOSS
+    <Router basename="/bumina"> 
       <div className="min-h-screen">
         <SocialWidget />
         <Header />
@@ -18,8 +19,6 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/blog" element={<Blog />} />
-            {/* Kalau nanti mau bikin halaman detail blog, tinggal tambahin di sini */}
-            {/* <Route path="/blog/:id" element={<DetailBlog />} /> */}
           </Routes>
         </main>
         <Footer />
