31b20c02e40118000f73b152384cb8a18daecef6
Ddos-spec <setgraph69@gmail.com>
2025-09-01 10:09:09 +0700

    kiirm

--- src/components/FloatingWidgets.tsx
+++ src/components/FloatingWidgets.tsx
@@ -32,7 +32,7 @@ const FloatingWidgets = () => {
 
   return (
     <div className="fixed bottom-6 right-6 z-50">
-      <div className="flex items-center space-x-4">
+      <div className="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4">
         {widgets.map((widget) => (
           <a
             key={widget.name}
